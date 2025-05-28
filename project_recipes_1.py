66d5da20990b92e0ad49915248a8f7f4efe3bfca
Olga <olgakuznetsova006@gmail.com>
2025-05-28 22:33:34 +0300

    Added parsing

--- project_recipes_1.py
+++ project_recipes_1.py
@@ -1,3 +1,37 @@
+# парсинг с сайта
+import requests
+from bs4 import BeautifulSoup
+
+HOST = "https://menunedeli.ru/"
+URL = 'https://menunedeli.ru/podborki-receptov/bystrye-recepty/'
+
+HEADERS = {'accept': '*/*', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'}
+
+
+def get_html(url, params=''):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+# в функции содержится предобработка ингредиентов
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('article', class_='post-card-in-lst row no-gutters')
+    recipes = []
+    for item in items:
+        recipes.append({
+            'Название': item.find('h5', class_='hdr').get_text(),
+            'Ингредиенты': item.find('ul', class_='ingredients-lst').get_text()
+        })
+    for el in recipes:
+        new_lst = el["Ингредиенты"].strip().split('\n')
+        del el["Ингредиенты"]
+        result = [i.split('–')[0].strip() for i in new_lst]
+        result_ = set(result)
+        el['Ингредиенты'] = result_
+    return recipes
+
+
 def find_possible_recipes(user_ingredients, recipes_site):
     recipes_all_ingredients = []
     possible_recipes_with_missing = []
